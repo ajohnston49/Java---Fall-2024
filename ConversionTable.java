2d15bb47486f5aa9c9fa3f841f73aecdbc2206e0
ajohnston49 <156711424+ajohnston49@users.noreply.github.com>
2024-08-23 17:01:44 -0400

    Newfile test2

---
+++ ConversionTable.java
@@ -0,0 +1,24 @@
+public class ConversionTable {
+
+    public static double footToMeter(double foot) {
+        return foot * 0.3048;
+    }
+
+    public static double meterToFoot(double meter) {
+        return meter / 0.3048;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Feet to Meters:");
+        System.out.println("Feet\tMeters");
+        for (int feet = 1; feet <= 10; feet++) {
+            System.out.printf("%d\t%.2f%n", feet, footToMeter(feet));
+        }
+
+        System.out.println("\nMeters to Feet:");
+        System.out.println("Meters\tFeet");
+        for (int meters = 20; meters <= 65; meters += 5) {
+            System.out.printf("%d\t%.2f%n", meters, meterToFoot(meters));
+        }
+    }
+}
