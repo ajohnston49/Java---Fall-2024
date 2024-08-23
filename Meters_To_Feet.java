5322577532b7845c9d9223f67363f5a053676403
ajohnston49 <156711424+ajohnston49@users.noreply.github.com>
2024-08-23 00:45:59 -0400

    commit change 1

---
+++ Meters_To_Feet.java
@@ -0,0 +1,11 @@
+public class Meters_To_Feet {
+    public static void main(string[]args) {
+        final double METERS_PER_FOOT = 0.305;
+        system.out.println("Feet/Meters");
+        system.out.println("------------");
+        for(int feet =1; feet <=10; feet++) {
+            double meters = feet * METERS_PER_FOOT;
+            system.out.printf("%d\t%.4f%n", feet, meters);
+        }
+    }
+}
